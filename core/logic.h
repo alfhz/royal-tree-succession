e1fa2e12f5a9abf111d5eeb1293ed57171965e08
ganesh diyan <khansaganesh@gmail.com>
2026-06-14 16:00:29 +0700

    perubahan minor

--- core/logic.h
+++ core/logic.h
@@ -1,1 +1,12 @@
 // Deklarasi class Tree, Insert, fungsi Traversal
+
+#ifndef LOGIC_H
+#define LOGIC_H
+#include "../data/royal.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Deklarasi fungsi untuk membuat node baru
+
+
+#endif 
